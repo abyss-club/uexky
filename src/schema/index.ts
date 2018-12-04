8eb400d1b5dda82ddd4248d443a20538e5c2c824
OrcaXS Suen <sun16281394@gmail.com>
2018-12-05 01:27:28 +0800

    Add schema for testing

---
+++ src/schema/index.ts
@@ -0,0 +1,6 @@
+export * from './base';
+export * from './notification';
+export * from './post';
+export * from './tag';
+export * from './thread';
+export * from './user';
---
+++ src/schema/user.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:max-line-length */
+const userQueries = `
+  # A user profile object.
+  profile(): User!
+`;
+
+const userMutations = `
+  # Register/Login via email address. An email containing login info will be sent to the provided email address.
+  auth(email: String!): Boolean!
+  # Set the Name of user.
+  setName(name: String!): User!
+  # Directly edit tags subscribed by user.
+  syncTags(tags: [String]!): User!
+  # Add tags subscribed by user.
+  addSubbedTags(tags: [String!]!): User!
+  # Delete tags subscribed by user.
+  delSubbedTags(tags: [String!]!): User!
+`;
+
+const userTypes = `
+  type User {
+    email: String!
+    # The Name of user. Required when not posting anonymously.
+    name: String
+    # Tags saved by user.
+    tags: [String!]
+  }
+`;
+
+export { userMutations, userQueries, userTypes };
