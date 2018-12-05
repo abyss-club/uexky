a596ff2dfd335b5f89143d7b84ef8d4c07654167
OrcaXS Suen <sun16281394@gmail.com>
2018-12-05 20:19:21 +0800

    Initial test for schema

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ console.log(schema);
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    profile: () => 'Hello world!',
   },
 };
 
