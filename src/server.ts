11090f3b84d8218c37250fcdc52b3ad990469ec3
OrcaXS Suen <sun16281394@gmail.com>
2018-12-06 23:00:45 +0800

    Add basic test

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,5 @@
-import { ApolloServer, gql } from 'apollo-server-koa';
-import { makeExecutableSchema } from 'graphql-tools';
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
-import * as mongoose from 'mongoose';
+import app from './app';
 
-import * as cors from '@koa/cors';
-
-import { schema } from './schema';
-
-mongoose.connect('mongodb://localhost:27017/testing', { useNewUrlParser: true });
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// console.log(schema);
-
-const server = new ApolloServer({
-  schema,
-});
-
-const app = new Koa();
-server.applyMiddleware({ app });
-
-app.use(async (ctx, next) => {
-    // Log the request to the console
-  console.log('Url:', ctx.url);
-    // Pass the request to the next middleware function
-  await next();
-});
-
-const router = new Router();
-router.get('/', async (ctx) => {
-  ctx.body = 'Hello World!';
-});
-
-app.use(router.routes());
-app.use(cors({ allowMethods: ['GET', 'OPTION', 'POST'] }));
-
-app.listen({ port: 5000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`),
-);
+const port = process.env.PORT || 5000;
+const server = app.listen(port);
+console.info(`Listening to http://localhost:${port} 🚀`);
