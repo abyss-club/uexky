a8822bc580d723f280e52f0f37f5507803074c47
OrcaXS Suen <sun16281394@gmail.com>
2018-12-07 09:44:48 +0800

    Update tests

---
+++ __tests__/routes.test.ts
@@ -0,0 +1,8 @@
+import * as request from 'supertest';
+import app from '../src/app';
+
+test('Hello API Request', async () => {
+  const response = await request(app.callback()).get('/');
+  expect(response.status).toEqual(200);
+  expect(response.text).toEqual('Hello World!');
+});
