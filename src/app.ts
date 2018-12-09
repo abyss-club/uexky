462bfece93442809d9d1a13612a0a177006ee147
OrcaXS Suen <sun16281394@gmail.com>
2018-12-09 23:33:10 +0800

    Move mongo from app to server

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,10 @@
 import { ApolloServer, gql } from 'apollo-server-koa';
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-import * as mongoose from 'mongoose';
 
 import * as cors from '@koa/cors';
 
 import { schema } from './schema';
-
-const { DB_PORT, DB_HOST, DB_NAME } = process.env;
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true });
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// console.log(schema);
 
 const server = new ApolloServer({
   schema,
