0c941bd478319937d0c6662a652b9c5408890141
OrcaXS Suen <sun16281394@gmail.com>
2018-12-09 23:33:10 +0800

    Move mongo from app to server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
+import * as mongoose from 'mongoose';
 import app from './app';
+
+const { DB_PORT, DB_HOST, DB_NAME } = process.env;
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true });
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port);
