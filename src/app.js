b39856f4423a92a389c1ec6099af51a720990bd6
OrcaXS Suen <sun16281394@gmail.com>
2018-12-11 23:53:14 +0800

    Add /auth to router

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ import 'module-alias/register';
 import { ApolloServer, gql } from 'apollo-server-koa';
 import Koa from 'koa';
 import Router from 'koa-router';
-
 import cors from '@koa/cors';
 
+import mailguntest from './mailgun';
 import { schema } from './schema';
 
 const server = new ApolloServer({
   schema,
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization || '';
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // add the user to the context
+    return { user };
+  },
 });
 
 const app = new Koa();
@@ -25,8 +33,12 @@ const router = new Router();
 router.get('/', async (ctx) => {
   ctx.body = 'Hello World!';
 });
+router.get('/auth', async (ctx) => {
+  ctx.body = 'Authentication!';
+});
 
 app.use(router.routes());
 app.use(cors({ allowMethods: ['GET', 'OPTION', 'POST'] }));
+// mailguntest();
 
 export default app;
