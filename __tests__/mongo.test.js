71266a69578404d47625d62cf958acd382e67732
OrcaXS Suen <sun16281394@gmail.com>
2018-12-12 21:30:00 +0800

    Update jest config, gitignore and eslintrc

--- __tests__/mongo.test.js
+++ __tests__/mongo.test.js
@@ -1,48 +1,29 @@
-import { MongoClient } from 'mongodb';
-import * as MongodbMemoryServer from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import MongoMemoryServer from 'mongodb-memory-server';
 
-const mongod = new MongodbMemoryServer.default({
-  autoStart: false,
-  instance: {
-    dbName: 'jest',
-  },
+// May require additional time for downloading MongoDB binaries
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
+let mongoServer;
+const opts = { useNewUrlParser: true };
+
+beforeAll(async () => {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getConnectionString();
+  await mongoose.connect(mongoUri, opts, (err) => {
+    if (err) console.error(err);
+  });
 });
 
-describe('insert', () => {
-  let connection;
-  let db;
+afterAll(() => {
+  mongoose.disconnect();
+  mongoServer.stop();
+});
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__);
-    db = await connection.db(global.__MONGO_DB_NAME__);
-  });
-
-  afterAll(async () => {
-    await connection.close();
-    await db.close();
-  });
-
-  it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
-
-    const mockUser = { _id: 'some-user-id', name: 'John' };
-    await users.insertOne(mockUser);
-
-    const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    expect(insertedUser).toEqual(mockUser);
-  });
-
-  it('should insert many docs into collection', async () => {
-    const users = db.collection('users');
-
-    const mockUsers = [{ name: 'Alice' }, { name: 'Bob' }];
-    await users.insertMany(mockUsers);
-
-    const insertedUsers = await users.find().toArray();
-    expect(insertedUsers).toEqual([
-      expect.objectContaining({ name: 'John' }),
-      expect.objectContaining({ name: 'Alice' }),
-      expect.objectContaining({ name: 'Bob' }),
-    ]);
+describe('...', () => {
+  it('...', async () => {
+    const User = mongoose.model('User', new mongoose.Schema({ name: String }));
+    const cnt = await User.count();
+    expect(cnt).toEqual(0);
   });
 });
--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,8 +1,18 @@
 import request from 'supertest';
 import app from '../src/app';
 
-test('Hello API Request', async () => {
-  const response = await request(app.callback()).get('/');
-  expect(response.status).toEqual(200);
-  expect(response.text).toEqual('Hello World!');
+describe('Testing paths', () => {
+  it('Get /invalid', async () => {
+    const response = await request(app.callback()).get('/invalid');
+    expect(response.status).toEqual(404);
+    expect(response.text).toEqual('Not Found');
+  });
+});
+
+describe('Testing auth', () => {
+  it('Plain request to /auth', async () => {
+    const response = await request(app.callback()).get('/auth');
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual('Incorrect authentication code');
+  });
 });
