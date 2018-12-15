fcc76375824ddf9452a17424d595b6eda7c1cb7a
OrcaXS Suen <sun16281394@gmail.com>
2018-12-16 00:15:37 +0800

    Implement auth and token

--- src/app.js
+++ src/app.js
@@ -6,12 +6,17 @@ import cors from '@koa/cors';
 
 import mailguntest from './mailgun';
 import { schema } from './schema';
+import { genRandomStr } from './utils/uuid';
+import { getUserByEmail } from './models/user';
+import { addToAuth, getEmailByCode } from './models/auth';
+import { getTokenByEmail } from './models/token';
 
 const server = new ApolloServer({
   schema,
   context: ({ req }) => {
     // get the user token from the headers
     const token = req.headers.authorization || '';
+    console.log(req);
     // try to retrieve a user with the token
     const user = getUser(token);
     // add the user to the context
@@ -33,8 +38,26 @@ const router = new Router();
 router.get('/', async (ctx) => {
   ctx.body = 'Hello World!';
 });
-router.get('/auth', async (ctx) => {
-  ctx.body = 'Authentication!';
+router.get('/auth', async (ctx, next) => {
+  try {
+    console.log(ctx.query.code);
+    const email = await getEmailByCode(ctx.query.code);
+    const token = await getTokenByEmail(email);
+    const expiry = new Date(token.createdAt);
+    expiry.setDate(expiry.getDate() + 20);
+    console.log(expiry);
+    ctx.body = token;
+    ctx.cookies.set('token', token.authToken, {
+      path: '/',
+      domain: process.env.API_DOMAIN,
+      httpOnly: true,
+      overwrite: true,
+      expires: expiry,
+    });
+  } catch (e) {
+    ctx.throw(401, e);
+  }
+  await next();
 });
 
 app.use(router.routes());
