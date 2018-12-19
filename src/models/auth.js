4f0d3743ef2c869f6dd2a196fb1d00a1c1773082
OrcaXS Suen <sun16281394@gmail.com>
2018-12-20 01:47:09 +0800

    Fix naming in auth model

--- src/models/auth.js
+++ src/models/auth.js
@@ -23,7 +23,7 @@ async function addToAuth(email) {
   const authCode = genRandomStr(36);
   const newAuth = { email, authCode, createdAt: new Date() };
   await AuthModel.update({ email }, newAuth, { upsert: true });
-  const res = await AuthModel.findOne({ email }, 'authCode').orFail(() => new Error('AuthCode Not Found'));
+  const res = await AuthModel.findOne({ email }, 'authCode').orFail(() => new Error('AuthCode not found for the email'));
   console.log(res);
   return res;
 }
@@ -31,6 +31,7 @@ async function addToAuth(email) {
 async function getEmailByCode(authCode) {
   const email = await AuthModel.findOne({ authCode });
   if (!email) throw new Error('Auth failed');
+  await AuthModel.deleteOne({ authCode }).orFail(() => new Error('Failed to invalidate authCode'));
   return email.email;
 }
 
