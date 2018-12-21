d04d5b4f38f954c13d807682014dfe80fba2a962
nanozuki <nanozuki.crows@gmail.com>
2018-12-21 13:13:46 +0800

    resolve tags

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'import/resolver': {
       alias: {
         map: [
-          ['~', '.']
+          ['~', './src']
         ],
         extensions: ['.js', '.jsx', '.json'],
       },
