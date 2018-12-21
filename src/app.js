e7ee21c92e24cae5e56a46e2170988062290eced
nanozuki <nanozuki.crows@gmail.com>
2018-12-21 13:13:46 +0800

    resolve tags

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import { ApolloServer, gql } from 'apollo-server-koa';
 import Koa from 'koa';
 import Router from 'koa-router';
--- src/models/tag.js
+++ src/models/tag.js
@@ -1,9 +1,33 @@
 import mongoose from 'mongoose';
+import config from '~/config';
 
 const TagSchema = mongoose.Schema({
-  name: String,
-  main_tags: [String],
-  update_time: Date(),
+  name: String, // unique
+  mainTags: [String],
+  updateAt: Date(),
 });
 const TagModel = mongoose.Model('Tag', TagSchema);
+
+TagSchema.statics.onPubThread = async function onPubThread(thread) {
+  thread.subTags.forEach(async (tag) => {
+    await TagModel.update({ name: tag }, {
+      $addToSet: { mainTags: thread.mainTag },
+      $set: { updatedAt: new Date() },
+    }, { upsert: true });
+  });
+};
+TagSchema.statics.getTree = async function getTree(limit) {
+  const defaultLimit = 10;
+  const tagTrees = config.mainTags.map(async (mainTag) => {
+    const subTags = await TagModel.find({ mainTags: mainTag })
+      .sort({ updatedAt: -1 })
+      .limit(limit || defaultLimit).all.exec();
+    return {
+      mainTag,
+      subTags: subTags.map(tag => tag.name),
+    };
+  });
+  return tagTrees;
+};
+
 export default TagModel;
