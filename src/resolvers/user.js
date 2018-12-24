8f756aa16853a215970e165208f00df5f4b98873
OrcaXS Suen <sun16281394@gmail.com>
2018-12-24 14:59:57 +0800

    Return context.user for profile query

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,17 +1,17 @@
 import UserModel from '../models/user';
 
-const User = (user) => {
-  profile: (obj, args, ctx, info) => {
-    console.log('resolver ctx', ctx);
-    return UserModel.profile(ctx);
-  }
-};
+// const User = (user) => {
+//   profile: (obj, args, ctx, info) => {
+//     console.log('resolver ctx', ctx);
+//     return UserModel.profile(ctx);
+//   }
+// };
 
 const profile = (ctx) => {
   console.log(ctx);
   if (!ctx.user) return null;
-  return UserModel.profile(ctx);
+  return ctx.user;
 };
 
-export default User;
+// export default User;
 export { profile };
