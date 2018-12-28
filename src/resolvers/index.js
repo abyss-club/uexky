6fea42e8bc351d5b44187352b0663e6a6744c77b
OrcaXS Suen <sun16281394@gmail.com>
2018-12-28 22:54:57 +0800

    Separate resolvers from schema

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,1 +1,7 @@
 import { merge } from 'lodash';
+import UserResolver from './user';
+import TagResolver from './tag';
+
+// const resolvers = Object.assign({}, UserResolver, TagResolver);
+const resolvers = merge(UserResolver, TagResolver);
+export default resolvers;
