3889fd1886943ce63cfd431ab1f0f7efd58612a9
OrcaXS Suen <sun16281394@gmail.com>
2019-01-03 00:28:39 +0800

    Update custom error handler

--- src/models/token.js
+++ src/models/token.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import isEmail from 'validator/lib/isEmail';
-import AuthFail from '~/error';
+import { AuthError } from '~/error';
 import { encode, decode, genRandomStr } from '../utils/uuid';
 
 const TokenSchema = new mongoose.Schema({
@@ -20,12 +20,12 @@ async function genNewToken(email) {
   const authToken = genRandomStr(24);
   const newToken = { email, authToken, createdAt: new Date() };
   await TokenModel.update({ email }, newToken, { upsert: true });
-  const result = await TokenModel.findOne({ email }).orFail(() => new AuthFail('AuthToken not found'));
+  const result = await TokenModel.findOne({ email }).orFail(() => new AuthError('AuthToken not found'));
   return result;
 }
 
 async function getEmailByToken(authToken) {
-  const result = await TokenModel.findOne({ authToken }).orFail(() => new AuthFail('Email not found'));
+  const result = await TokenModel.findOne({ authToken }).orFail(() => new AuthError('Email not found'));
   return result.email;
 }
 
