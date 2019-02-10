8fdba862fce8280b6db816c4f6e7a3d12c4fc3ab
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-02-10 22:23:15 +0800

    add redis and impl rete-limit

--- src/rate-limit/model.js
+++ src/rate-limit/model.js
@@ -4,7 +4,7 @@ const RateLimiterSchema = new mongoose.Schema({
   ip: String,
   email: String,
   mutable: Boolean,
-  updatedAt: Date,
+  createdAt: Date,
   remaining: Number,
 });
 const RateLimiterModel = mongoose.model('rate_limit', RateLimiterSchema);
@@ -12,5 +12,28 @@ const RateLimiterModel = mongoose.model('rate_limit', RateLimiterSchema);
 // TODO: add ttl for limiter
 // TODO: cut down limiters' remaining
 // TODO: read settings from config
+//
+RateLimiterSchema.statics.take = async function take(
+  ctx, cost, mutable, ip, email = '',
+) {
+  const selector = { ip, mutable };
+  if (email !== '') {
+    selector.email = email;
+  }
+  const config = await ctx.config.getRateLimit();
+  const now = new Date();
+  const remainingInit = mutable ? config.MutLimit : config.QueryLimit;
+  const rl = await RateLimiterModel.findOneAndUpdate(
+    selector,
+    {
+      $setOnInsert: { ...selector, createdAt: now, remaining: remainingInit },
+      $dec: { remaining: cost },
+    },
+    { new: true, upsert: 1 },
+  ).exec();
+  if (rl.remaining < 0) {
+    throw new Error('rate limit execceed!');
+  }
+};
 
 export default RateLimiterModel;
