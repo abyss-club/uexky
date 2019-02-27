17650c2669441c59ebad7f9e878a3f557541004a
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-02-27 16:58:19 +0800

    test models/config

--- src/models/config.js
+++ src/models/config.js
@@ -7,9 +7,9 @@ import log from '~/utils/log';
 const ConfigSchema = new mongoose.Schema({
   rateLimit: {
     httpHeader: String,
-    queryLimit: String,
+    queryLimit: Number,
     queryResetTime: Number,
-    mutLimit: String,
+    mutLimit: Number,
     mutResetTime: Number,
   },
   rateCost: {
@@ -19,17 +19,17 @@ const ConfigSchema = new mongoose.Schema({
   },
 }, { capped: 1 });
 
-const rateCostObjSchema = Joi.object().keys({
-  CreateUser: Joi.number().integer().min(0).default(30),
-  PubThread: Joi.number().integer().min(0).default(10),
-  PubPost: Joi.number().integer().min(0).default(1),
-});
 const rateLimitObjSchema = Joi.object().keys({
-  HTTPHeader: Joi.string().alphanum().default(''),
-  QueryLimit: Joi.number().integer().min(0).default(300),
-  QueryResetTime: Joi.number().integer().min(0).default(3600),
-  MutLimit: Joi.number().integer().min(0).default(30),
-  MutResetTime: Joi.number().integer().min(0).default(3600),
+  httpHeader: Joi.string().regex(/^[0-9a-zA-Z-]*/).default(''),
+  queryLimit: Joi.number().integer().min(0).default(300),
+  queryResetTime: Joi.number().integer().min(0).default(3600),
+  mutLimit: Joi.number().integer().min(0).default(30),
+  mutResetTime: Joi.number().integer().min(0).default(3600),
+});
+const rateCostObjSchema = Joi.object().keys({
+  createUser: Joi.number().integer().min(0).default(30),
+  pubThread: Joi.number().integer().min(0).default(10),
+  pubPost: Joi.number().integer().min(0).default(1),
 });
 const configObjSchema = Joi.object().keys({
   rateLimit: rateLimitObjSchema.default(rateLimitObjSchema.validate({}).value),
@@ -39,28 +39,35 @@ const configObjSchema = Joi.object().keys({
 ConfigSchema.statics.getConfig = async function getConfig() {
   const results = await ConfigModel.find({}).exec();
   if (results.length === 0) {
-    return configObjSchema.validate({});
+    return configObjSchema.validate({}).value;
   }
-  return results[0];
+  return results[0].format();
 };
 
 ConfigSchema.statics.setConfig = async function setConfig(input) {
   const config = await ConfigModel.getConfig();
-  Object.keys(config).forEach((key) => {
-    Object.assign(config[key], input[key]);
+  Object.keys(input).forEach((key) => {
+    config[key] = Object.assign(config[key] || {}, input[key] || {});
   });
+  log.info('will to set new config', config);
   const { error, value: newConfig } = configObjSchema.validate(config);
   if (error) {
     log.error(error);
     throw new ParamsError(`JSON validation failed, ${error}`);
   }
-  await ConfigModel.updateOne({}, newConfig).exec();
+  await ConfigModel.updateOne({}, newConfig, { upsert: true }).exec();
+  log.info('config updated!', newConfig);
   return newConfig;
+};
+
+ConfigSchema.methods.format = function format() {
+  const obj = this.toObject();
+  return (({ rateLimit, rateCost }) => ({ rateLimit, rateCost }))(obj);
 };
 
 const ConfigModel = mongoose.model('Config', ConfigSchema);
 
-const getConfig = () => {
+const genConfigReader = () => {
   let configValue = null;
   return async () => {
     if (!configValue) {
@@ -71,4 +78,4 @@ const getConfig = () => {
 };
 
 export default ConfigModel;
-export { getConfig };
+export { genConfigReader };
