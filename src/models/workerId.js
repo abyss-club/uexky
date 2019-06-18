d938ea0d9251ec93624c577ade4b98713801d0e4
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-06-18 12:36:16 +0800

    transfer uid

--- src/models/workerId.js
+++ src/models/workerId.js
@@ -9,10 +9,10 @@ import { query } from '~/utils/pg';
 const WorkerIdModel = () => ({
   newWorkerId: async function newWorkerId() {
     const results = await query(
-      'INSERT INTO counter (name) VALUES (\'worker\') ON CONFLICT (name)'
-      + 'DO UPDATE SET count = counter.count + 1 RETURNING count;',
+      "INSERT INTO counter (name) VALUES ('worker') ON CONFLICT (name)"
+      + 'DO UPDATE SET count = counter.count + 1 RETURNING counter.count;',
     );
-    return results.rows[0].id % 512;
+    return results.rows[0].count % 512;
   },
 });
 
