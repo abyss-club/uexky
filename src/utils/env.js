75b994c242d75b3e6994ddf48cbd02bed2358030
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-07-14 10:26:04 +0800

    bug-fix

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,9 +2,9 @@ const env = {
   PG_URI: process.env.PG_URI || 'postgresql://localhost',
   REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379/0',
 
-  MAINGUN_PRIVATE_KEY: process.env.MAINGUN_PRIVATE_KEY || 'private_key',
-  MAINGUN_PUBLIC_KEY: process.env.MAINGUN_PUBLIC_KEY || 'public_key',
-  MAINGUN_DOMAIN: process.env.MAINGUN_DOMAIN || 'mail.abyss.club',
+  MAILGUN_PRIVATE_KEY: process.env.MAILGUN_PRIVATE_KEY || 'private_key',
+  MAILGUN_PUBLIC_KEY: process.env.MAILGUN_PUBLIC_KEY || 'public_key',
+  MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN || 'mail.abyss.club',
 
   DOMAIN: process.env.DOMAIN,
   API_DOMAIN: process.env.API_DOMAIN,
